a49303d2fd6cd152f43f51c84982b5b3d6b950b8
SimtionR <simtionrazvan7@gmail.com>
2022-03-24 15:48:40 +0200

    Generic

---
+++ Generics and Collections/GenericsAndCollections/GenericAndCollections.ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using GenericsAndCollections.Library;
+
+var  stringCollection= new GenericCollection<string>
+    (new string[] {"first", "second", "third", "forth", "fifth","sixth"});
+
+
+var s1= stringCollection.GetItem(3);
+Console.WriteLine(s1);
+
+stringCollection.SetItem("Not forth anymore", 3);
+
+var s2 = stringCollection.GetItem(3);
+Console.WriteLine(s2);
+
+var index0 = stringCollection.GetItem(0);
+var index3 = stringCollection.GetItem(3);
+Console.WriteLine($"Before swap index 0 :{index0} - index 3: {index3}");
+
+
+
+stringCollection.SwapItemsByIndex(0, 3);
+
+index0 = stringCollection.GetItem(0);
+index3 = stringCollection.GetItem(3);
+
+
+Console.WriteLine($"After swap index 0 :{index0} - index 3: {index3}");
+
+
+var index1 = stringCollection.GetItem(1);
+var index5 =stringCollection.GetItem(5);
+
+Console.WriteLine($"Before swap index 1 :{index1} - index 5: {index5}");
+
+stringCollection.SwapItems("second", "sixth");
+
+index1 = stringCollection.GetItem(1);
+index5= stringCollection.GetItem(5);
+
+Console.WriteLine($"After swap index 1 :{index1} - index 5: {index5}");
+
+
---
+++ Generics and Collections/GenericsAndCollections/GenericsAndCollections.Library/GenericCollection.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAndCollections.Library
+{
+    public class GenericCollection<T> : ISetItem<T>, IGetItem<T>, ISwapItems<T>
+    {
+        private readonly int _maximumSize;
+        private T[] _items;
+
+        public GenericCollection(T[] items)
+        {
+            _items = items;
+            _maximumSize = items.Length;
+        }
+
+        public T GetItem(int index)
+        {
+            if(index <0)
+            {
+                throw new InvalidOperationException();
+            }
+            if(index >= _maximumSize)
+            {
+                throw  new IndexOutOfRangeException();
+            }
+
+
+            return _items[index];
+            
+        }
+
+        public void SetItem(T item, int index)
+        {
+            if (index < 0)
+            {
+                throw new InvalidOperationException();
+            }
+            if (index >= _maximumSize)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            _items[index] = item;
+        }
+
+        public void SwapItems(T firstItem, T secondItem)
+        {
+            var firstIndex = Array.FindIndex(_items, x => x.Equals(firstItem));
+
+            var secondIndex =Array.FindIndex(_items, x => x.Equals(secondItem));
+
+            _items[firstIndex] = secondItem;
+            _items[secondIndex] = firstItem;
+        }
+
+        public void SwapItemsByIndex(int firstItemIndex, int secondItemIndex)
+        {
+
+            if (firstItemIndex < 0 || secondItemIndex < 0)
+            {
+                throw new InvalidOperationException();
+            }
+            if(firstItemIndex >= _maximumSize || secondItemIndex >= _maximumSize)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+
+            var firstItem = _items[firstItemIndex];
+            var secondItem = _items[secondItemIndex];
+
+
+            _items[secondItemIndex] = firstItem;
+            _items[firstItemIndex] = secondItem;
+        }
+    }
+}
